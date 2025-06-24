2bfff56bdca19e7b9d32d28cfe54da02dc3a32ba
shayanabbas <shayanabbas@outlook.com>
2025-06-24 23:24:41 +0300

    feat: add RESTful API routes for TaskController with auth protection

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+use App\Http\Controllers\Api\TaskController;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::get('/tasks/{task}', [TaskController::class, 'show']);
+    Route::put('/tasks/{task}', [TaskController::class, 'update']);
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+}); 
